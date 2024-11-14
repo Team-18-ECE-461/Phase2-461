f894159d610d2a53500a9310620d1e522442a6e6
ssrini160 <140915709+ssrini160@users.noreply.github.com>
2024-11-14 14:07:22 -0500

    Revert "Merge branch 'main' of https://github.com/Team-18-ECE-461/Phase2-461"
    This reverts commit 4c06591e1896a69791e2baade0c65da4753bb65b, reversing
    changes made to 0574eb04112befb19f26d2114c2154929810f787.

--- lambda/packagesubendpoints.ts
+++ lambda/packagesubendpoints.ts
@@ -67,7 +67,7 @@ async function handleGetPackage(packageId: string) {
         }
 
         const s3Client = new S3();
-        const key = `${packageId}/${packageVersion}`;
+        const key = `${packageId}-${packageVersion}`;
         const param = {
             Bucket: BUCKET_NAME,
             Key: key
@@ -113,8 +113,8 @@ async function handleUpdatePackage(event: LambdaEvent) {
         const packageId = requestBody.metadata.ID;
         const packageName = requestBody.metadata.Name;
         const packageVersion = requestBody.metadata.Version;
-        const content = requestBody.data.content;
-        const url = requestBody.data.url;
+        let content = requestBody.data.content;
+        let url = requestBody.data.url;
         const JSProgram = requestBody.data.JSProgram;
         const currentTime = Date.now();
 
@@ -127,109 +127,128 @@ async function handleUpdatePackage(event: LambdaEvent) {
         }
 
         const checkParams = {
-            TableName: TABLE_NAME,
-            Key: {
-              ID: { S: packageId }
-            }
-          };
+          TableName: TABLE_NAME,
+          IndexName: "ID-index", // Specify the GSI name here
+          KeyConditionExpression: "ID = :packageId",
+          ExpressionAttributeValues: {
+              ":packageId": { S: packageId }
+          }
+      };
       
-        const checkResult = await dynamoDBclient.send(new GetItemCommand(checkParams));
-      
-        if (!checkResult.Item) {
+        const checkResult = await dynamoDBclient.send(new QueryCommand(checkParams));
+        
+        if (!checkResult.Items) {
             return { statusCode: 404, body: JSON.stringify({ message: "Package not found" }) };
         }
 
-        const CreatedAt = checkResult.Item.CreatedAt.S;
-        if (!CreatedAt) {
-            throw new Error("CreatedAt is undefined");
-        }
-        const isNewer = await isNewerPackage(CreatedAt, packageName);
-        const s3 = new S3();
-
-        if(isNewer){
-            //create new version in dynamoDB and s3
-            const newitem = {
-                ID: { S: packageId },
-                Name: { S: packageName },
-                Version: { S: packageVersion },
-                JSProgram: { S: JSProgram },
-                CreatedAt: { S: new Date().toISOString() },
-                };
-            await dynamoDBclient.send(new PutItemCommand({
-                TableName: TABLE_NAME,
-                Item: newitem,
-            }));
-
-            if(content){
-                const zipBuffer = Buffer.from(content, 'base64');
-                const key = `${packageName}/${packageVersion}`;
-                await s3.putObject({
-                    Bucket: BUCKET_NAME,
-                    Key: key,
-                    Body: zipBuffer,
-                    ContentType: 'application/zip',
-                });
-            }
-            else if (url) {
-                const key = `${packageId}/${packageVersion}`;
-                const response = await axios.get(`${url}/archive/refs/heads/main.zip`, { responseType: 'arraybuffer' });
-                const zipBuffer = Buffer.from(response.data);
-                const urlcontent = zipBuffer.toString('base64');
-                await s3.putObject({
-                    Bucket: BUCKET_NAME,
-                    Key: key,
-                    Body: zipBuffer,
-                    ContentType: 'application/zip',
-                });
-            }       
-
+        const versionExists = checkResult.Items.some(item => item.Version && item.Version.S === packageVersion);
+        if(versionExists)
+        {
+            return { statusCode: 400, body: JSON.stringify({ message: "Version already exists" }) };
         }
 
-        else {
-            
+
+        if(content){
+          if (checkResult.Items.some(item => item.URL)) { //check if the package has a URL(was not uploaded by content)
+            return { statusCode: 400, body: JSON.stringify({ message: "Invalid request" }) };
+          }
+
+          const mostRecentVersion  = await getMostRecentVersion(packageName);
+          if (mostRecentVersion && !checkValidVersionContent(packageVersion, mostRecentVersion)) {  //check for invalid patch
+            return { statusCode: 400, body: JSON.stringify({ message: "Invalid version" }) };
+          }
+        }
+        if(url){
+          if(!checkResult.Items.some(item => item.URL)) { //check if the package has a URL(was not uploaded by content)
+            return { statusCode: 400, body: JSON.stringify({ message: "Invalid request" }) };
+          }
         }
 
-        //update package in dynamoDB
-        const item = {
-            ID: { S: packageId },
-            Name: { S: packageName },
-            Version: { S: packageVersion },
-            JSProgram: { S: JSProgram },
-            CreatedAt: { S: new Date().toISOString() },
+        if(debloat){
+          const tempDir = await fs.promises.mkdtemp(path.join(tmpdir(), 'package-'));
+          const packagedebloatName = packageName;
+          const version = packageVersion;
+          let packagePath = ''
+          if(content){
+            packagePath =  await downloadAndExtractNpmPackage(url, tempDir);
+          }
+          if(url){
+            packagePath = await extractBase64ZipContent(content, tempDir);
+          }
+          const outputDir = path.join(tempDir, 'debloated');
+          fs.mkdirSync(outputDir, { recursive: true });
+          let entryPath = getEntryPoint(path.join(packagePath, 'package.json'));
+          entryPath = entryPath ? path.join(packagePath, entryPath) : null;
+          if (!entryPath) {
+            return {
+              statusCode: 400,
+              body: JSON.stringify('No entry point found'),
             };
-        
-        await dynamoDBclient.send(new PutItemCommand({
-        TableName: TABLE_NAME,
-        Item: item,
-        }));
+          }
+          await esbuild.build({
+            entryPoints: [entryPath], 
+            bundle: false,
+            outdir: outputDir,
+            minify: true,
+            treeShaking: true,
+        });
 
-        //update package in S3
-
-        if (content) {
-            const zipBuffer = Buffer.from(content, 'base64');
-            const key = `${packageName}/${packageVersion}`;
-            await s3.putObject({
-                Bucket: BUCKET_NAME,
-                Key: key,
-                Body: zipBuffer,
-                ContentType: 'application/zip',
-            });
-        }
-        else if (url) {
-            const key = `${packageId}/${packageVersion}`;
-            const response = await axios.get(`${url}/archive/refs/heads/main.zip`, { responseType: 'arraybuffer' });
-            const zipBuffer = Buffer.from(response.data);
-            //const urlcontent = zipBuffer.toString('base64');
-            await s3.putObject({
-                Bucket: BUCKET_NAME,
-                Key: key,
-                Body: zipBuffer,
-                ContentType: 'application/zip',
-            });
-        }
-
+    const debloatedZipPath = path.join(tempDir, 'debloated.zip');
+    await zipFolder(outputDir, debloatedZipPath);
+    const uploadkey = `${packagedebloatName}-${version}`;
+    const debloatID = generatePackageId(packagedebloatName, version);
+    const zipBuffer = fs.readFileSync(debloatedZipPath);
+    const base64Zip = zipBuffer.toString('base64');
+    await uploadToS3(debloatedZipPath, BUCKET_NAME, uploadkey);
+    await cleanupTempFiles(tempDir);
+    await uploadDB(debloatID, packagedebloatName, version, JSProgram, url);
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        metadata: {
+          Name: packagedebloatName,
+          Version: version,
+          ID: debloatID,
+        },
+        data: {
+          Content: base64Zip,
+          URL: url, 
+          JSProgram: JSProgram,
+        },
+      }),
+    };
+  }
+  else{
+    let zipBuffer: Buffer;
+    if (content) {
+      zipBuffer = Buffer.from(content, 'base64');
     }
-    catch (error) {
+   else if (url) {
+      url = await urlhandler(url);
+      const response = await axios.get(`${url}/archive/refs/heads/main.zip`, { responseType: 'arraybuffer' });
+      zipBuffer = Buffer.from(response.data);
+    } else { 
+      throw new Error('No content or URL provided');
+    }
+
+    const packageId = generatePackageId(packageName, packageVersion);
+    let key = `${packageName}-${packageVersion}`;
+    await s3.putObject({
+      Bucket: BUCKET_NAME,
+      Key: key,
+      Body: zipBuffer,
+      ContentType: 'application/zip',
+    });
+    uploadDB(packageId, packageName, packageVersion, JSProgram, url);   
+    return {
+      statusCode: 200,
+      body: "Package updated successfully",
+    }; 
+
+  }
+  }
+
+  catch (error) {
         console.error('Error updating package:', error);
         return { statusCode: 500, body: JSON.stringify({ message: 'Error updating package' }) };
     }
@@ -293,23 +312,209 @@ async function getMostRecentVersion(packageName: string) {
       }
 }
 
-async function isNewerPackage(IDCreatedAt: string, packageName:string ) {
-    // Convert the IDCreatedAt timestamp from DynamoDB to a number
-    const IDCreatedAtTimestamp = new Date(IDCreatedAt).getTime();
-    
-    // Get the CreatedAt timestamp of the most recent version in ISO string format
-    const mostRecentVersionCreatedAt = await getMostRecentVersion(packageName);
-    if(!mostRecentVersionCreatedAt) {
-        throw new Error("mostRecentVersionCreatedAt is undefined");
-    }
-    const mostRecentVersionTimestamp = new Date(mostRecentVersionCreatedAt).getTime();
-    
-    // Compare the timestamps
-    if (IDCreatedAtTimestamp > mostRecentVersionTimestamp) {
-        return true; // IDCreatedAt is newer
-    } else {
-        return false; // mostRecentVersionCreatedAt is newer or the same
-    }
+async function checkValidVersionContent(version:string, mostRecentVersion:number) {
+  const [major, minor, patch] = version.split('.').map(Number);
+  const mostRecentMajor: number = Math.floor((mostRecentVersion) / 1000000);
+  const mostRecentMinor = Math.floor((mostRecentVersion % 1000000) / 1000);
+  const mostRecentPatch = mostRecentVersion % 1000;
+
+  
+
+  // If the major and minor versions are the same, check for sequential patch updates
+  if (major === mostRecentMajor && minor === mostRecentMinor) {
+    return patch > mostRecentPatch;
+  }
+
+  // Otherwise, the incoming version is not valid
+  return true;
 }
 
+async function urlhandler(url:string){
+  if(url.includes('github')){
+    const repoNameMatch = url.match(/github\.com\/[^\/]+\/([^\/]+)/);
+    if (repoNameMatch && repoNameMatch[1]) {
+      return url ;
+    }
+  }
+  else if(url.includes('npm')){
+    try {
+      const match = url.match(/\/package\/([^\/]+)/); // Match the pattern
+      const packageName=  match ? match[1] : null
+      const response = await axios.get(`https://registry.npmjs.org/${packageName}`, { responseType: 'json' });
+      const repositoryUrl = response.data.repository?.url;
+      if (repositoryUrl) {
+        url = getgithuburl(repositoryUrl);
+      } else {
+        throw new Error('No repository URL found');
+      }
+      const repoNameMatch = url.match(/github\.com\/[^\/]+\/([^\/]+)/);
+      if (repoNameMatch && repoNameMatch[1]) {
+        return url;
+      }
+    } catch (error) {
+      console.error(`Error fetching package data: ${error}`);
+    }
+    
+  }
+  }
+  function getEntryPoint(packageJsonPath: string): string | null {
+    const packageJson = require(packageJsonPath);
+    if (packageJson.main) {
+      const mainPath = path.join(path.dirname(packageJsonPath), packageJson.main);
+      if (fs.existsSync(mainPath)) {
+          return packageJson.main;
+      }
+   } 
+    // Check for index.js first
+    const indexPath = path.join(path.dirname(packageJsonPath), 'index.js');
+    if (fs.existsSync(indexPath)) {
+      return 'index.js';
+    }  
+    const indextPath = path.join(path.dirname(packageJsonPath), 'index.ts');
+    if (fs.existsSync(indextPath)) {
+      return 'index.ts';
+    }  
+    // If index.js doesn't exist, check the bin category
+    if (packageJson.bin && typeof packageJson.bin === 'object') {
+      const binFiles = Object.values(packageJson.bin);
+      if (binFiles.length > 0 && fs.existsSync(path.join(path.dirname(packageJsonPath), binFiles[0] as string))) {
+        return binFiles[0] as string;
+      }
+    }  
+    if (packageJson.browser) {
+      const browserPath = path.join(path.dirname(packageJsonPath), packageJson.browser);
+      if (fs.existsSync(browserPath)) {
+          return packageJson.browser;
+      }
+    } 
+    // If nothing found in bin, check the files category (if defined)
+    if (packageJson.files && packageJson.files.length > 0) {
+      for (const file of packageJson.files) {
+        const filePath = path.join(path.dirname(packageJsonPath), file);
+        if (fs.existsSync(filePath)) {
+          return file;
+        }
+      }
+    }
+    //If nothing found in files, check the exports category (if defined)
+    if(packageJson.exports){
+      const entryPoint = packageJson.exports['.'] || packageJson.exports['./index.js'];
+      if(entryPoint){
+        return entryPoint;
+      }
+    }
+    if (packageJson.module) {
+      const modulePath = path.join(path.dirname(packageJsonPath), packageJson.module);
+      if (fs.existsSync(modulePath)) {
+          return packageJson.module;
+      }
+  }
+// If no entry point is found, return null
+    return null;
+  }
+
+  async function uploadDB(packageId: string, packageName: string, packageVersion: string, JSProgram: string, url: string){
+    const item = {
+      ID: { S: packageId },
+      Name: { S: packageName },
+      Version: { S: packageVersion },
+      VersionInt: { N: versionInt(packageVersion).toString() },
+      JSProgram: { S: JSProgram },
+      CreatedAt: { S: new Date().toISOString() },
+      URL: { S: url },
+    };
+  
+    await dynamoDBclient.send(new PutItemCommand({
+      TableName: TABLE_NAME,
+      Item: item
+    }));
+  
+  }
+  function versionInt(version: string): number{
+    const [major, minor, patch] = version.split('.').map(Number);
+    return major * 1000000 + minor * 1000 + patch;
+  }
+  async function downloadAndExtractNpmPackage(npmUrl: string, destination: string): Promise<any> {
+    // Convert npm URL to registry API URL
+    const packageName = npmUrl.split('/').pop();
+    const registryUrl = `https://registry.npmjs.org/${packageName}`;
+  
+    // Fetch package metadata
+    const response = await axios.get(registryUrl);
+    const latestVersion = response.data['dist-tags'].latest;
+    const tarballUrl = response.data.versions[latestVersion].dist.tarball;
+  
+    // Download the tarball
+    const tarballPath = path.join(destination, 'package.tgz');
+    const writer = fs.createWriteStream(tarballPath);
+    const downloadResponse = await axios.get(tarballUrl, { responseType: 'stream' });
+    await new Promise((resolve, reject) => {
+        downloadResponse.data.pipe(writer);
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+    });
+  
+    // Extract tarball
+    await tar.extract({ file: tarballPath, cwd: destination });
+    return [path.join(destination, 'package'), latestVersion, packageName]; // Adjust this based on the extracted directory structure
+  }
+  
+  
+  function generatePackageId(name: string, version: string): string {
+    const hash = crypto.createHash('sha256');
+    hash.update(`${name}-${version}`);
+    return hash.digest('hex').slice(0, 12); // Truncate for a shorter ID
+  }
+  
+  // Function to clean up temporary files
+  async function cleanupTempFiles(tempFilePath: string): Promise<void> {
+    try {
+      // Check if the path exists
+      await fs.promises.access(tempFilePath);
+      const stats = await fs.promises.stat(tempFilePath);
+  
+      // Remove file or directory as needed
+      if (stats.isFile()) {
+        await fs.promises.unlink(tempFilePath);
+      } else if (stats.isDirectory()) {
+        await fs.promises.rm(tempFilePath, { recursive: true, force: true });
+      }
+    } catch (error) {
+      console.error(`Error cleaning up temporary file or directory at ${tempFilePath}:`, error);
+    }
+  }
+  async function extractBase64ZipContent(base64Content: string, destination: string): Promise<string> {
+    const zipPath = path.join(destination, 'package.zip');
+    const buffer = Buffer.from(base64Content, 'base64');
+    await fs.promises.writeFile(zipPath, buffer);
+  
+    // Extract zip content
+    await fs.createReadStream(zipPath)
+        .pipe(unzipper.Extract({ path: destination }))
+        .promise();
+  
+    return path.join(destination, 'package'); // Adjust based on zip structure
+  }
+  async function zipFolder(source: string, out: string) {
+    const archive = fs.createWriteStream(out);
+    const zipper = archiver('zip', {
+        zlib: { level: 9 }  // High compression level
+    });
+  
+    zipper.pipe(archive);
+    zipper.directory(source, false);
+    await zipper.finalize();
+  }
+
+  async function uploadToS3(filePath: string, bucketName: string, key: string) {
+    const fileStream = fs.createReadStream(filePath);
+    await s3.putObject({
+        Bucket: bucketName,
+        Key: key,
+        Body: fileStream,
+        ContentType: 'application/zip',
+    })
+  
+  }
+  
 
