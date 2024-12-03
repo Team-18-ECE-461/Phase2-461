3fee6d016355eaf103f1dd49aa0bebed08d06ffa
Shriya Srinivas <140915709+ssrini160@users.noreply.github.com>
2024-12-03 11:47:27 -0500

    Update reportWebVitals.ts

--- authentication/authentication-app/src/reportWebVitals.ts
+++ authentication/authentication-app/src/reportWebVitals.ts
@@ -2,11 +2,11 @@ import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
   if (onPerfEntry && typeof onPerfEntry === 'function') {
-    getCLS(onPerfEntry);
-    getFID(onPerfEntry);
-    getFCP(onPerfEntry);
-    getLCP(onPerfEntry);
-    getTTFB(onPerfEntry);
+    onCLS(onPerfEntry);
+    onFID(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
   }
 };
 
