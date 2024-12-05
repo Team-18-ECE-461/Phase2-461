5da36c3640c36ed5b42bbac576d1236475a984fb
ssrini160 <140915709+ssrini160@users.noreply.github.com>
2024-12-05 17:28:13 -0500

    added message

--- authentication/authentication-app/src/pages/UploadPage.tsx
+++ authentication/authentication-app/src/pages/UploadPage.tsx
@@ -101,7 +101,7 @@ const UploadPage: React.FC = () => {
       } else {
         console.error('Unexpected upload error:', error);
       }
-      setMessage('Failed to upload package or fetch data. Please try again.' + error);
+      setMessage('Failed to upload package or fetch data. Please try again.' + error.message);
 
     }
   };
